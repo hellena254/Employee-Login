aff3187967391c9aaece7555b8ac8baac0027aef
Hellen Atieno <hellenah243@gmail.com>
2023-07-10 10:23:44 +0300

    Updated the client-side by implementing responsive forms

--- Client/LoginForm/src/app/app.module.ts
+++ Client/LoginForm/src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -17,8 +21,13 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Client/LoginForm/src/app/login/login.component.ts
+++ Client/LoginForm/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { LoginService } from './login.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -9,26 +11,41 @@ import { LoginService } from './login.service';
 })
 
 export class LoginComponent {
-  username: string = '';
-  password: string = '';
+  form: FormGroup;
+  hidePassword: boolean =true;
 
+  constructor(private fb: FormBuilder, private http: HttpClient){
+    this.form = this.fb.group({
+      id: [],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
 
-  constructor(private loginService: LoginService){}
+  // password visibility 
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  // Submit form 
+  onSubmit() {
+    if(this.form.valid) {
+      const formData = this.form.value;
+
+      this.http.post("http://localhost:8080/api/employee", formData).subscribe(
+        response => {
+          console.log('Success:', response);
+          this.form.reset();
+        },
+        error => {
+          console.error('Error:', error)
+        }
+      ); 
+    }
+  }
+
    
   
-  isLoggedIn: boolean = false;
-
-  login(): void {
-    this.loginService.login(this.username, this.password).subscribe(
-      response => {
-        // Handle successful login response
-        console.log("Login successful:", response);
-        this.isLoggedIn = true;
-      },
-      error => {
-        // Handle login error
-        console.error("Login error:", error);
-      }
-    );
-  }
+  
+  
 }
